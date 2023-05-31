b5180a7bfe76365bb6031c3a25cda4a935aa6a37
Truffaconfrutilla <121985452+Truffaconfrutilla@users.noreply.github.com>
2023-05-31 14:29:18 -0400

    clase 31/05

--- Florculia/urls.py
+++ Florculia/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('core.urls')),   
-    path('accounts/', include('django.contrib.auth.urls')),
-    
+    path('accounts/', include('django.contrib.auth.urls')),    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
